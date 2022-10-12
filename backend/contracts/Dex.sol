06c118a3422da58166dc7e73b8f57bc54552837e
Justina Quak <justinaquak@gmail.com>
2022-10-13 00:04:10 +0800

    add dex functions

--- backend/contracts/Dex.sol
+++ backend/contracts/Dex.sol
@@ -7,11 +7,93 @@ contract Dex {
 
   ERC20 public token;
 
-  event Bought(uint256 amount);
-  event Sold(uint256 amount);
+  mapping (uint => TokenStore) tokenStores;
+  mapping (string => uint) reverseTokenStore;
+  uint tokenIndex;
+  mapping(address => mapping(uint => uint)) tokenBalanceForAddress;
+
+  struct TokenStore {
+    address contractAddress;
+    string symbolName;
+    OrderBook sellOrderBook;
+    OrderBook buyOrderBook;
+    uint orderCount;
+  }
+
+  struct OrderBook {
+    mapping (uint => MarketOrder) marketOrders; 
+  }
+
+  struct MarketOrder {
+    string symbol;
+    address owner;
+    uint amount;
+  }
 
   constructor(string memory _name, string memory _symbol) {
     token = new Token(_name, _symbol);
+    tokenIndex = 0;
+  }
+
+  // Helper functions
+  // hasToken
+  // getBalanceForToken
+
+  // Add Token into tokenStore
+  function addToken(string memory _symbolName, address _contractAddress) public returns (uint) {
+     TokenStore storage tokenStore;
+     tokenStore.contractAddress = _contractAddress;
+     tokenStore.symbolName = _symbolName;
+     tokenStore.sellOrderBook = new OrderBook;
+     tokenStore.buyOrderBook = new OrderBook;
+     tokenStore.orderCount = 0;
+     tokenStores[tokenIndex] = tokenStore;
+     
+     reverseTokenStore[_symbolName] = tokenIndex;
+     tokenIndex++;
+     return tokenIndex;
+  }
+
+  // Deposit Token
+  function depositToken(string memory _symbolName, uint amount) public {
+    // TODO require msg.sender has token, and sufficient balance
+    uint temp = reverseTokenStore[_symbolName];
+    tokenBalanceForAddress[msg.sender][temp] += amount;
+  }
+
+  // Withdraw Token
+  function withdrawToken(string memory _symbolName, uint amount) public {
+    // TODO require msg.sender has token, and sufficient balance
+    uint temp = reverseTokenStore[_symbolName];
+    tokenBalanceForAddress[msg.sender][temp] -= amount;
+  }
+
+  // Create orders
+  function createOrder(string memory buySymbol, uint buyAmount, string memory sellSymbol, uint sellAmount) public {
+    // TODO check for price condition
+    uint buySymbolIndex = reverseTokenStore[buySymbol];
+    uint orderIndex = tokenStores[buySymbolIndex].orderCount;
+    createBuyOrder(buySymbolIndex, orderIndex, buySymbol, buyAmount);
+    createSellOrder(buySymbolIndex, orderIndex, buySymbol, sellSymbol, sellAmount);
+    tokenStores[buySymbolIndex].orderCount++;
+  }
+
+  function createBuyOrder(uint buySymbolIndex, uint orderIndex, string memory buySymbol, uint buyAmount) public {
+    MarketOrder memory currMarketOrder;
+    currMarketOrder.symbol = buySymbol;
+    currMarketOrder.amount = buyAmount;
+    currMarketOrder.owner = msg.sender;
+    
+    tokenStores[buySymbolIndex].buyOrderBook[orderIndex] = currMarketOrder;
+  }
+
+  function createSellOrder(uint buySymbolIndex, uint orderIndex, string memory buySymbol, string memory sellSymbol, uint sellAmount) public {
+    MarketOrder memory currMarketOrder;
+    currMarketOrder.symbol = sellAmount;
+    currMarketOrder.amount = sellAmount;
+    currMarketOrder.owner = msg.sender;
+    
+    tokenStores[buySymbolIndex].sellOrderBook[orderIndex] = currMarketOrder;
   }
 
   function buy() payable public {
@@ -20,7 +102,7 @@ contract Dex {
     require(amountTobuy > 0, "You need to send some ether");
     require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
     token.transfer(msg.sender, amountTobuy);
-    emit Bought(amountTobuy);
+    // emit Bought(amountTobuy);
   }
 
   function sell(uint256 amount) public {

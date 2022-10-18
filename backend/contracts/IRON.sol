c84b45001af96c5c7e655d4a96c855100cf80215
Justina Quak <justinaquak@gmail.com>
2022-10-18 23:39:03 +0800

    contracts

--- backend/contracts/IRON.sol
+++ backend/contracts/IRON.sol
@@ -26,13 +26,13 @@ contract IRON is ERC20 {
     balanceOfAddress[msg.sender] = totalSupply; // Give the creator all initial tokens
   }
 
-  function allowance(address _owner, address _spender) external view returns (uint256) {
-    return allowed[_owner][_spender];
+  function allowance(address _from, address _to) external view returns (uint256) {
+    return allowed[_from][_to];
   }
 
-  function approve(address _owner, address _spender, uint256 _value) external returns (bool success) {
-    allowed[_owner][_spender] = allowed[_owner][_spender].add(_value);
-    emit Approval(_owner, _spender, _value, allowed[_owner][_spender]);
+  function approve(address _from, address _to, uint256 _value) external returns (bool success) {
+    allowed[_from][_to] = allowed[_from][_to].add((_value.mul(1e18)));
+    emit Approval(_from, _to, (_value.mul(1e18)), allowed[_from][_to]);
     return true;
   }
 
@@ -41,28 +41,28 @@ contract IRON is ERC20 {
   }
 
   function transfer(address _to, uint256 _numberOfTokens) external returns (bool) {
-    require(balanceOfAddress[msg.sender] >= _numberOfTokens);
-    balanceOfAddress[msg.sender] -= _numberOfTokens;
-    balanceOfAddress[_to] += _numberOfTokens;
-    emit Transfer(msg.sender, _to, _numberOfTokens);
+    require(balanceOfAddress[msg.sender] >= (_numberOfTokens.mul(1e18)));
+    balanceOfAddress[msg.sender] -= (_numberOfTokens.mul(1e18));
+    balanceOfAddress[_to] += (_numberOfTokens.mul(1e18));
+    emit Transfer(msg.sender, _to, (_numberOfTokens.mul(1e18)));
     return true;
   }
 
   function transferFrom(address _from, address _to, uint256 _tokens) external returns (bool) {
-    uint256 allowedAmount = allowed[_from][msg.sender];
-    require(balanceOfAddress[_from] >= _tokens && allowedAmount >= _tokens);
-    balanceOfAddress[_to] += _tokens;
-    balanceOfAddress[_from] -= _tokens;
+    uint256 allowedAmount = allowed[_from][_to];
+    require(balanceOfAddress[_from] >= (_tokens.mul(1e18)) && allowedAmount >= (_tokens.mul(1e18)));
+    balanceOfAddress[_to] += (_tokens.mul(1e18));
+    balanceOfAddress[_from] -= (_tokens.mul(1e18));
     if (allowedAmount < MAX_UINT256) {
-      allowed[_from][msg.sender] -= _tokens;
+      allowed[_from][_to] -= (_tokens.mul(1e18));
     }
-    emit Transfer(_from, _to, _tokens); 
+    emit Transfer(_from, _to, (_tokens.mul(1e18))); 
     return true;
   }
 
-  function reduceAllowance(address _owner, address _spender, uint256 _value) external returns (uint256 currentAllowance) {
-    allowed[_owner][_spender] = allowed[_owner][_spender].sub(_value);
-    emit Approval(_owner, _spender, _value, allowed[_owner][_spender]);
-    return allowed[_owner][_spender];
+  function reduceAllowance(address _from, address _to, uint256 _value) external returns (uint256 currentAllowance) {
+    allowed[_from][_to] = allowed[_from][_to].sub((_value.mul(1e18)));
+    emit Approval(_from, _to, (_value.mul(1e18)), allowed[_from][_to]);
+    return allowed[_from][_to];
   }
 }

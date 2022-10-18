c6eb249224e6aa78ace129162eb6efdd9b6499cf
Goh Hong Xiang, Bryan <72204360+brygoh@users.noreply.github.com>
2022-10-19 00:23:29 +0800

    Frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,42 @@
 import React from 'react'
-import Card from '../components/Card';
+import { Select, Input } from 'antd';
+import 'antd/dist/antd.min.css';
 
 import NavBar from '../components/NavBar';
+import Card from '../components/Card';
 import './styles.css';
+
+const option = [
+  {value: 'IRON', label: 'IRON'},
+  {value: 'GOLD', label: 'GOLD'},
+  {value: 'SILVER', label: 'SILVER'},
+  {value: 'BRONZE', label: 'BRONZE'},
+]
 
 function Home() {
 
-  function Test() {
+  function ConnectToMetaWallet() {
     return (
-      <div>hello</div>
+      <div className="home-connect-meta-wallet">
+        <span>You haven't connected to a wallet</span>
+        <button style={{marginLeft: '16px'}}>Connect Wallet</button>
+      </div>
+    )
+  }
+
+  function SwapTokens() {
+    return (
+      <div className="home-swap-token">
+        <div>
+          <Select className='select' options={option}/>
+          <Input className='input' />
+        </div>
+        <img alt="swap-coins" src="./swap.png" style={{height: '64px'}}/>
+        <div>
+          <Select className='select' options={option}/>
+          <Input className='input' />
+        </div>
+      </div>
     )
   }
 
@@ -16,10 +44,8 @@ function Home() {
     <div className="home-big-daddy">
       <NavBar />
       <div className="home-content-daddy">
-        <Card content={Test()} />
-        <Card content={Test()} />
-        <Card content={Test()} />
-        <Card content={Test()} />
+        <Card content={ConnectToMetaWallet()} />
+        <Card content={SwapTokens()} />
       </div>
     </div>
   );

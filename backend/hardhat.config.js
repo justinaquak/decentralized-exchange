43d474a447fe0134571ff6601dd1c3775ce78de5
Goh Hong Xiang, Bryan <72204360+brygoh@users.noreply.github.com>
2022-10-23 21:34:40 +0800

    config

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -21,9 +21,11 @@ module.exports = {
     localhost: {
       url: `http://127.0.0.1:8545/`,
       chainId: 31337,
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"],
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"],
       gas: 8e6,
       gasPrice: 8e9,
+      gasLimit: 1e10,
+      allowUnlimitedContractSize: true,
     }
   },
   solidity: {
